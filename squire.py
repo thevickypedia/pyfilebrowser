db5318d30bfcba1252422188ccf8b30aba03f949
thevickypedia <vignesh.sivanandarao@gmail.com>
2024-03-17 11:49:45 -0500

    Kick off and start serving

---
+++ squire.py
@@ -0,0 +1,54 @@
+import os
+from typing import List
+
+import bcrypt
+from pydantic import BaseModel, DirectoryPath, FilePath
+
+from modals.config import ConfigSettings
+from modals.users import UserSettings
+
+
+def hash_password(password):
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    return hashed_password.decode('utf-8')
+
+
+def validate_password(password, hashed_password):
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password.encode('utf-8'))
+
+
+def remove_trailing_underscore(dictionary):
+    if isinstance(dictionary, dict):
+        for key in list(dictionary.keys()):
+            if isinstance(dictionary[key], dict):
+                dictionary[key] = remove_trailing_underscore(dictionary[key])
+            if key.endswith('_'):
+                new_key = key.rstrip('_')
+                dictionary[new_key] = dictionary.pop(key)
+    elif isinstance(dictionary, list):
+        for i, item in enumerate(dictionary):
+            dictionary[i] = remove_trailing_underscore(item)
+    return dictionary
+
+
+class EnvConfig(BaseModel):
+    """Configure all env vars and validate using ``pydantic`` to share across modules.
+
+    >>> EnvConfig
+
+    """
+
+    user_settings: List[UserSettings] = [UserSettings()]
+    config_settings: ConfigSettings = ConfigSettings()
+
+
+class FileIO(BaseModel):
+    base_dir: DirectoryPath = os.path.join('settings')
+    config: FilePath = os.path.join(base_dir, 'config.json')
+    users: FilePath = os.path.join(base_dir, 'users.json')
+
+
+fileio = FileIO()
+
+if not os.path.isdir(fileio.base_dir):
+    os.mkdir(fileio.base_dir)
