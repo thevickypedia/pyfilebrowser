b60d227d17beda2759bd6c8c6a8b92d88efe7e83
thevickypedia <vignesh.sivanandarao@gmail.com>
2024-03-17 11:30:06 -0500

    Handle config settings

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import json
-import os.path
 
 import bcrypt
 
-from settings import EnvConfig, admin_perm, default_perm
+from modals.models import admin_perm, default_perm
+from settings import EnvConfig, fileio
 
 
 def hash_password(password):
@@ -15,30 +15,55 @@ def validate_password(password, hashed_password):
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password.encode('utf-8'))
 
 
-class Generator:
+def remove_trailing_underscore(dictionary):
+    if isinstance(dictionary, dict):
+        for key in list(dictionary.keys()):
+            if isinstance(dictionary[key], dict):
+                dictionary[key] = remove_trailing_underscore(dictionary[key])
+            if key.endswith('_'):
+                new_key = key.rstrip('_')
+                dictionary[new_key] = dictionary.pop(key)
+    elif isinstance(dictionary, list):
+        for i, item in enumerate(dictionary):
+            dictionary[i] = remove_trailing_underscore(item)
+    return dictionary
+
+
+class FileBrowser:
     def __init__(self, **kwargs):
         self.env = EnvConfig(**kwargs)
 
-    def handle_users(self):
+    def create_users(self):
         final_settings = []
         for idx, setting in enumerate(self.env.user_settings):
             if setting.authentication.admin:
-                setting.authentication.perm = admin_perm()
+                setting.perm = admin_perm()
             else:
-                setting.authentication.perm = default_perm()
+                setting.perm = default_perm()
             hashed_password = hash_password(setting.authentication.password)
             assert validate_password(setting.authentication.password, hashed_password), "Validation failed!"
             setting.authentication.password = hashed_password
-            user_settings = json.loads(setting.model_dump_json())
-            user_settings['id'] = idx + 1
-            user_settings['authentication'].pop('admin', None)  # remove custom values inserted
-            user_settings.update(user_settings['authentication'])
-            user_settings.pop('authentication', None)  # remove custom values inserted
+            model_settings = json.loads(setting.model_dump_json())
+            user_settings = {'id': idx + 1}
+            model_settings['authentication'].pop('admin', None)  # remove custom values inserted
+            user_settings.update(model_settings['authentication'])
+            model_settings.pop('authentication', None)  # remove custom values inserted
+            user_settings.update(model_settings)
             final_settings.append(user_settings)
-        with open(os.path.join('settings', 'config.json'), 'w') as file:
-            json.dump(final_settings, file)
+        with open(fileio.users, 'w') as file:
+            json.dump(final_settings, file, indent=4)
+            file.flush()
+
+    def create_config(self):
+        if str(self.env.config_settings.settings.branding.files) == ".":
+            self.env.config_settings.settings.branding.files = ""
+        final_settings = remove_trailing_underscore(json.loads(self.env.config_settings.model_dump_json()))
+        with open(fileio.config, 'w') as file:
+            json.dump(final_settings, file, indent=4)
+            file.flush()
 
 
 if __name__ == '__main__':
-    generator = Generator()
-    generator.handle_users()
+    file_browser = FileBrowser()
+    file_browser.create_users()
+    file_browser.create_config()
---
+++ modals/config.py
@@ -0,0 +1,148 @@
+import os
+import socket as sock
+from enum import StrEnum
+from typing import List, Optional, Union
+
+from pydantic import DirectoryPath, PositiveInt, BaseModel, FilePath
+from pydantic_settings import BaseSettings
+
+from modals.models import Sorting, Perm, default_perm
+
+
+class Defaults(BaseSettings):
+    scope: str = "."
+    locale: str = "en"
+    viewMode: str = "list"
+    singleClick: bool = False
+    sorting: Sorting = Sorting()
+    perm: Perm = default_perm()
+    commands: List = []
+    hideDotfiles: bool = False
+    dateFormat: bool = False
+
+    class Config:
+        """Environment variables configuration."""
+
+        env_prefix = "defaults_"
+        env_file = os.environ.get("env_file", os.environ.get("ENV_FILE", ".config.env"))
+        extra = "ignore"
+
+
+class Theme(StrEnum):
+    light: str = "light"
+    dark: str = "dark"
+    blank: str = ""
+
+
+class Branding(BaseSettings):
+    name: str = ""
+    disableExternal: bool = False
+    disableUsedPercentage: bool = False
+    files: Optional[DirectoryPath] = ""
+    theme: Theme = Theme.blank
+    color: str = ""
+
+    class Config:
+        """Environment variables configuration."""
+
+        env_prefix = "branding_"
+        env_file = os.environ.get("env_file", os.environ.get("ENV_FILE", ".config.env"))
+        extra = "ignore"
+
+
+class Tus(BaseSettings):
+    chunkSize: PositiveInt = 10 * 1024 * 1024  # Defaults to 10 MB
+    retryCount: PositiveInt = 5
+
+    class Config:
+        """Environment variables configuration."""
+
+        env_prefix = "tus_"
+        env_file = os.environ.get("env_file", os.environ.get("ENV_FILE", ".config.env"))
+        extra = "ignore"
+
+
+class Commands(BaseSettings):
+    after_copy: List[str] = []
+    after_delete: List[str] = []
+    after_rename: List[str] = []
+    after_save: List[str] = []
+    after_upload: List[str] = []
+    before_copy: List[str] = []
+    before_delete: List[str] = []
+    before_rename: List[str] = []
+    before_save: List[str] = []
+    before_upload: List[str] = []
+
+    class Config:
+        """Environment variables configuration."""
+
+        env_prefix = "commands_"
+        env_file = os.environ.get("env_file", os.environ.get("ENV_FILE", ".config.env"))
+        extra = "ignore"
+
+
+class Log(StrEnum):
+    stdout: str = "stdout"
+    file: str = "file"
+
+
+class Config(BaseSettings):
+    signup: bool = False
+    createUserDir: bool = False
+    userHomeBasePath: str = os.path.join(os.path.expanduser('~'), 'users')
+    defaults: Defaults = Defaults()
+    authMethod: str = "json"
+    branding: Branding = Branding()
+    tus: Tus = Tus()
+    commands: Commands = Commands()
+    shell_: List[str] = []
+    rules: List[str] = []
+
+    class Config:
+        """Environment variables configuration."""
+
+        env_prefix = ""
+        env_file = os.environ.get("env_file", os.environ.get("ENV_FILE", ".config.env"))
+        extra = "ignore"
+
+
+class Server(BaseSettings):
+    root: DirectoryPath
+    baseURL: str = ""
+    socket: str = ""
+    tlsKey: Union[FilePath, str] = ""
+    tlsCert: Union[FilePath, str] = ""
+    port: PositiveInt = 8080
+    address: str = sock.gethostbyname('localhost')
+    log: Log = Log.stdout
+    enableThumbnails: bool = False
+    resizePreview: bool = False
+    enableExec: bool = False
+    typeDetectionByHeader: bool = False
+    authHook: str = ""
+    tokenExpirationTime: str = ""
+
+    class Config:
+        """Environment variables configuration."""
+
+        env_prefix = ""
+        env_file = os.environ.get("env_file", os.environ.get("ENV_FILE", ".config.env"))
+        extra = "ignore"
+
+
+class Auther(BaseSettings):
+    recaptcha: Optional[str] = None
+
+    class Config:
+        """Environment variables configuration."""
+
+        env_prefix = "auth_"
+        env_file = os.environ.get("env_file", os.environ.get("ENV_FILE", ".config.env"))
+        extra = "ignore"
+
+
+class ConfigSettings(BaseModel):
+    settings: Config = Config()
+    server: Server = Server()
+    auther: Auther = Auther()
